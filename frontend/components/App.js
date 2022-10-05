345899e677014d82383aa139fa4dccab0ae63850
Branden Jones <branden.wayne.jones@gmail.com>
2022-10-05 12:52:06 -0400

    getting todos from server and mounting to DOM

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  // constructor(){
+  //   super();
+  //   this.state = {
+
+  //   }
+  // }
+state = {
+  todos: [],
+}
+
+fetchAllTodos = () => {
+  axios.get(URL)
+    .then(res => {
+      this.setState({...this.state, todos: res.data.data})
+    })
+    .catch(err =>
+      console.error(`NOOOO`, err)
+    )
+}
+
+componentDidMount() {
+  this.fetchAllTodos();
+}
+
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>No Errors</div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(task => {
+              return <div key={task.id}>{task.name} </div>
+            })
+          }
+        </div>
+        <form id='todoForm'>
+          <input placeholder='Enter New Task' type='text' />
+          <button>Add Task</button>
+        </form>
+        <button>Hide Completed</button>
+      </div>
+    )
   }
 }
